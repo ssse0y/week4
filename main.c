7b001054d284500a9626e09b173b80a3026c9087
ssse0y <ko527ko@gmail.com>
2022-09-28 20:19:13 +0900

    #3

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int s, t, m;
+	int s, m, s1;
 	
 	printf("input the second:");
 	scanf("%i",&s);
-	t=s/60;
-	m=s%60;
+	m=s/60;
+	s1=s%60;
 
-	printf("the time is %i : %i",t,m);
+	printf("the time is %i : %i",m,s1);
 }
